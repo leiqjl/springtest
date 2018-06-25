8aec7c89985ef04df3b9701d776aae2fd30013a5
leiqjl <leiqjl@gmail.com>
2018-06-26 00:01:41 +0800

    ..

--- src/main/java/com/leiqjl/spittr/web/HomeController.java
+++ src/main/java/com/leiqjl/spittr/web/HomeController.java
@@ -1,13 +1,14 @@
 package com.leiqjl.spittr.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping({"/", "/homepage"})
 public class HomeController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping
     public String home() {
         return "home";
     }
